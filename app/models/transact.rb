164fe6f52033c6ce00763d57b05d85dbbc21eedf
daudi13 <davouma13@gmail.com>
2022-07-04 18:41:54 +0300

    Add validation for transact

--- app/models/transact.rb
+++ app/models/transact.rb
@@ -1,3 +1,7 @@
 class Transact < ApplicationRecord
   belongs_to:categories
+
+  validates :name, presence: true
+  validates :amout, presence: true, numericality: { greater_than: 0 }
+  validates :category_id, presence: true
 end
