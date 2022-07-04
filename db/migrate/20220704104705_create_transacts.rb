8564d4c7e356caab668194141137630ba02ede55
daudi13 <davouma13@gmail.com>
2022-07-04 18:42:54 +0300

    Add validation for transact

--- db/migrate/20220704104705_create_transacts.rb
+++ db/migrate/20220704104705_create_transacts.rb
@@ -1,7 +1,7 @@
 class CreateTransacts < ActiveRecord::Migration[7.0]
   def change
     create_table :transacts do |t|
-      t.references :author, null: false, foreign_key: {to_table: :users }
+      t.references :category, null: false, foreign_key: true
       t.string :name
       t.float :amount
 
