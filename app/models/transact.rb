5e2b226bf5e5eaa2a203b3bf57d168fcc643b55d
daudi13 <davouma13@gmail.com>
2022-07-05 15:13:26 +0300

    Add models

--- app/models/transact.rb
+++ app/models/transact.rb
@@ -1,7 +1,7 @@
 class Transact < ApplicationRecord
-  belongs_to:categories
+  belongs_to :category
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :amout, presence: true, numericality: { greater_than: 0 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :category_id, presence: true
 end
